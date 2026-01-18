1f9262cf8cdbb0fba8b4cd6a88003603f585ef7c
Shahil-Dev <alfee9006@gmail.com>
2026-01-18 23:16:35 +0600

    six commit

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import postRouter from './modules/post/Post.router.js';
+import { auth } from './lib/auth.js';
+import { toNodeHandler } from "better-auth/node";
+
 
 const app = express();
 
+app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use(express.json());
 
 app.use('/posts', postRouter);
